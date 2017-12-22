12beac72b247423953400e9a2e0cc3225c8ec1f0
py-task <2529716798@qq.com>
2017-12-22 14:08:43 +0800

    完善服务

--- ERPClient/src/main/java/rmi/RemoteHelper.java
+++ ERPClient/src/main/java/rmi/RemoteHelper.java
@@ -1,6 +1,6 @@
 package rmi;
 
-import service.blservice.ClassifyBLService;
+import service.blservice.*;
 
 import java.rmi.Remote;
 
@@ -21,5 +21,53 @@ public class RemoteHelper {
 
     public ClassifyBLService getClassifyBLService(){
         return (ClassifyBLService)remote;
+    }
+
+    public CommodityBLService getCommodityBLService(){
+        return (CommodityBLService)remote;
+    }
+
+    public GoodsBLService getGoodsBLService(){
+        return (GoodsBLService)remote;
+    }
+
+    public GoodsSaleBLService getGoodsSaleBLService(){
+        return (GoodsSaleBLService)remote;
+    }
+
+    public GoodsSaleReturnBLService getGoodsSaleReturnBLService(){
+        return (GoodsSaleReturnBLService)remote;
+    }
+
+    public GoodsStockBLService getGoodsStockBLService(){
+        return (GoodsStockBLService)remote;
+    }
+
+    public GoodsStockReturnBLService getGoodsStockReturnBLService(){
+        return (GoodsStockReturnBLService)remote;
+    }
+
+    public MemberBLService getMemberBLService(){
+        return (MemberBLService)remote;
+    }
+
+    public SaleBLService getSaleBLService(){
+        return (SaleBLService)remote;
+    }
+
+    public SaleReturnBLService getSaleReturnBLService(){
+        return (SaleReturnBLService)remote;
+    }
+
+    public StockBLService getStockBLService(){
+        return (StockBLService)remote;
+    }
+
+    public StockReturnBLService getStockReturnBLService(){
+        return (StockReturnBLService)remote;
+    }
+
+    public UserBLService getUserBLService(){
+        return (UserBLService)remote;
     }
 }
